5d18052204add35f5cbe4456d6f52a2b7479d946
Kevin Glasgow <kglas001@gmail.com>
2020-09-19 03:37:23 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,28 @@
-# require modules here
+require 'pry'
+require "yaml"
 
-def load_library
+emoticons = YAML.load_file('./lib/emoticons.yml')
+
+def load_library(emoticons)
+  emoticons = YAML.load_file('./lib/emoticons.yml')
+  emoticons.each_with_object({}) do |(feeling, emotes), sorted_list|
+    if !sorted_list[feeling]
+      sorted_list[feeling] = {
+        :english => "",
+        :japanese => ""
+        
+      }
+    end
+    sorted_list[feeling][:english] << emotes[0]
+    sorted_list[feeling][:japanese] << emotes[1]
+  end
+end
+binding.pry
+def get_japanese_emoticon(emoticons, eng_emote)
   # code goes here
 end
 
-def get_japanese_emoticon
+def get_english_meaning(emoticons, jap_emote)
   # code goes here
 end
 
-def get_english_meaning
-  # code goes here
-end
